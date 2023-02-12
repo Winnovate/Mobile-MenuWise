5ac5bca5e935aea734e8f9a720d2f7ebe74a14fa
Tapan Patel <tapan2930@gmail.com>
2023-02-11 23:46:28 -0500

    @feature/Adding all the screen - qrcode, menu, items in app Navigator

--- src/navigators/appNavigator.js
+++ src/navigators/appNavigator.js
@@ -6,6 +6,8 @@ import HomeScreen from '../screens/home';
 import SettingsScreen from '../screens/settings';
 import AppointmentScreen from '../screens/appointment';
 import QrScannerScreen from '../screens/qrScanner';
+import MenuCatScreen from '../screens/menuCat';
+import MenuItmesScreen from '../screens/items';
 
 
 
@@ -55,9 +57,31 @@ const AppNavigator = () => {
             },
           }}
         />
+
+        <Stack.Screen
+          name="MenuCat"
+          component={MenuCatScreen}
+          options={{
+            title: 'Menu',
+            headerTintColor: colors.text,
+            headerStyle: {
+              backgroundColor: colors.background,
+            },
+          }}
+        />
+
+        <Stack.Screen
+          name="Items"
+          component={MenuItmesScreen}
+          options={{
+            title: 'Menu',
+            headerTintColor: colors.text,
+            headerStyle: {
+              backgroundColor: colors.background,
+            },
+          }}
+        />
       </Stack.Navigator>
-
-
     </>
   );
 };
