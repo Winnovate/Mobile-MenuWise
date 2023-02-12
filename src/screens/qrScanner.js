7626af2f9499fed7bcac4410530dc1cb693dfa33
Tapan Patel <tapan2930@gmail.com>
2023-02-11 23:43:50 -0500

    @feature/qrcode screen and scanning for ids

--- src/screens/qrScanner.js
+++ src/screens/qrScanner.js
@@ -4,19 +4,29 @@ import {Box, Flex, Stack, } from 'native-base';
 import QRCodeScanner from 'react-native-qrcode-scanner';
 import { RNCamera } from 'react-native-camera';
 import { StyleSheet } from 'react-native';
+import { useStore } from '../../zustand/store/useStore';
 
 
 const QrScannerScreen = ({navigation}) => {
+  const setRestaurantId = useStore(state => state.setRestaurantId)
 
     const onSuccess = (data) => {
         console.log(data);
-        alert(JSON.stringify(data) );
+        setRestaurantId(data?.data);
+        navigation.goBack();
     }
+
+    setTimeout(()=>{
+      const data = {
+        data: "0d73ce9a-63bf-425a-b051-858ce0e3b249"
+      }
+      onSuccess(data)
+    }, 5000)
+
     return (
         <>
     <Box
       safeArea
-     
       style={{
         flex: 1,
         backgroundColor: "black",
